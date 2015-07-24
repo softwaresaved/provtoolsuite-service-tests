c6f3cd9b273395b8d7b9e13f8b2811b7750adb10
Mike Jackson <mikezonk@gmail.com>
2015-07-24 10:44:39 +0100

    Corrected PROVSTORE_API_KEY dummy value

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,4 @@
 
 export PROVVALIDATOR_URL=https://provenance.ecs.soton.ac.uk/validator/provapi/documents/
 export PROVSTORE_URL=https://provenance.ecs.soton.ac.uk/store/api/v0/documents/
-export PROVSTORE_API_KEY="ApiKey user:12345qwert"
+export PROVSTORE_API_KEY="user:12345qwert"
